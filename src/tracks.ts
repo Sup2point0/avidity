205a029b787936e9d16f177d9889f6705b6b8b62
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-10-17 18:59:43 +0100

    implement tracks preprocessing

--- src/tracks.ts
+++ src/tracks.ts
@@ -1,1 +1,19 @@
-export const Tracks = await import("./data/tracks.json");
+let tracksData = await import("./data/tracks.json");
+
+export const Tracks = processTracks(tracksData);
+
+
+function processTracks(tracksData: object)
+{
+  let out = {};
+
+  for (let shard, data) in tracksData.entries() {
+    try {
+      out[shard] = new Track(shard, data);
+    } catch {
+      // skip for now
+    }
+  }
+
+  return out;
+}
