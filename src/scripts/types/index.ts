8bd921314a58bbf56b0f46c5f067b7162229c2f3
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-10-18 10:17:25 +0100

    update `types/index`

--- src/scripts/types/index.ts
+++ src/scripts/types/index.ts
@@ -1,2 +1,6 @@
 export { Track } from "./track";
 export { Playlist } from "./playlist";
+
+export { NavData } from "./nav-data";
+export { PlaybackData } from "./playback-data";
+export { SearchData } from "./search-data";
