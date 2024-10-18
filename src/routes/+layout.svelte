ceb9f51a3fba86501802a21648ae731071066154
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-10-18 10:09:00 +0100

    add keyboard shortcuts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,44 +7,61 @@ import Nav from "#parts/nav.svelte";
 
 function onKeydown(event)
 {
-  if (event.repeat) return;
-}
+  if (event.ctrlKey || event.metaKey) {
+    switch (event.key) {
+      case " ":
+        return playbackExec.playNext();
+  
+      case "f":
+      case "/":
+        return;  // TODO focus search
+    }
+  }
+  else {
+    switch (event.key) {
+      case " ":
+      case "k":
+        if (event.repeat) return;
+        return playbackExec.togglePause();
 
-function onKeyup(event)
-{
-  if (event.repeat) return;
+      case "ArrowLeft":
+        return playbackExec.shift(-5);
+      case "ArrowRight":
+        return playbackExec.shift(+5);
+  
+      case "j:
+        return playbackExec.shift(-10);
+      case "l":
+        return playbackExec.shift(+10);
+    }
+  }
 }
 
 </script>
 
 
-<svelte:window
-  on:keydown={onKeydown}
-  on:keyup={onKeyup}
-/>
+<svelte:window on:keydown={onKeydown} />
 
 
-<div id="ui-layout-page">
-
-<Nav>
-
-<div id="ui-layout-split">
-  <section style:flex-grow={0.5}>
-    <TrackDetails>
-  </section>
-
-  <section style:flex-grow={1}>
-    <slot> Uh, something went wrong! </slot>
-  </section>
-
-  <section style:flex-grow={0.5}>
-    <PlaybackDetails>
-  </section>
-</div>
-
-<TrackControls>
-
-</div>
+<main id="ui-layout-page">
+  <Nav>
+  
+  <div id="ui-layout-split">
+    <section style:flex-grow={0.5}>
+      <TrackDetails>
+    </section>
+  
+    <section style:flex-grow={1}>
+      <slot> Uh, something went wrong! </slot>
+    </section>
+  
+    <section style:flex-grow={0.5}>
+      <PlaybackDetails>
+    </section>
+  </div>
+  
+  <TrackControls>
+</main>
 
 
 <style lang="scss">
