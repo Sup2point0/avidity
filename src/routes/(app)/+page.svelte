3c36d59f6f0256a09913c3bd2ff122cc9a4993a1
Sup#2.0 <sup2point0@gmail.com>
2024-10-20 21:28:33 +0100

    debug playback

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,9 +2,36 @@
 
 import TrackRow from "#parts/track-row.svelte";
 
+import playback_executive from "#scripts";
+import { playback } from "#scripts/stores";
+
 </script>
 
 
 <TrackRow track="dawn" />
 <TrackRow track="day" />
 <TrackRow track="terabyte" />
+
+
+<section>
+  <h2> debug </h2>
+  <pre>playback_executive: {JSON.stringify(playback_executive, null, 2)} </pre>
+  <pre>$playback: {JSON.stringify($playback, null, 2)} </pre>
+</section>
+
+
+<style lang="scss">
+  section {
+    padding: 1rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: white;
+    background-color: rgb(0 0 0 / 80%);
+
+    > * {
+      margin: 1rem;
+      display: block;
+    }
+  }
+</style>
