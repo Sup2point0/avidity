37b5f27dd8d060bd6b53726306bd52f2b9d4485b
Sup#2.0 <sup2point0@gmail.com>
2024-10-20 12:09:08 +0100

    add `TrackControls`

---
+++ src/parts/track-controls.svelte
@@ -0,0 +1,128 @@
+<script>
+
+import playback_executive from "#scripts";
+import { playback } from "#scripts/stores";
+
+
+$: track = $playback.current;
+
+</script>
+
+
+<div class="part">
+  <div class="track-controls">
+
+    <section id="playback-controls">
+      <button class="ui rounded"
+        on:click={playback_executive.restart}
+        disabled={!playback_executive.playing}
+      >
+        <span class="material-symbols-rounded"> arrow_left </span>
+      </button>
+
+      <button class="ui rounded"
+        on:click={playback_executive.toggle_pause}
+        disabled={!playback_executive.playing}
+      >
+        <span class="material-symbols-rounded">
+          {#if playback_executive.playing?.paused}
+            play
+          {:else}
+            pause
+          {/if}
+        </span>
+      </button>
+
+      <button class="ui rounded"
+        on:click={playback_executive.play_next}
+        disabled={!playback_executive.playing}
+      >
+        <span class="material-symbols-rounded"> arrow_right </span>
+      </button>
+    </section>
+
+    <section id="playback-details">
+      <div id="track-details">
+        <h4> {track?.name ?? ""} </h4>
+        <p> {track?.artist ?? ""} </p>
+      </div>
+
+      <div id="playback-progress">
+        <p> {playback_executive.elapsed ?? "--:--"} </p>}
+        <meter
+          min={0} max={1}
+          value={playback_executive.playing ? (playback_executive.elapsed / track.duration) : 0}
+        >
+        <p> {track?.duration ?? "--:--"} </p>
+      </div>
+    </section>
+
+  </div>
+</div>
+
+
+<style lang="scss">
+
+@use './src/styles/mixins/ui' as *;
+
+
+.part {
+  width: 100%;
+  min-height: 2rem;
+  @include elevated-block;
+}
+
+.track-controls {
+  margin-left: auto;
+  width: 90%;
+  height: 100%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+section#playback-controls {
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+
+section#playback-details {
+  flex-grow: 1;
+
+  #track-details {
+    > * {
+      display: block;
+    }
+
+      h4 {
+        @include font-flavour;
+        color: var(--col-text);
+      }
+      p {
+        @include font-ui;
+        color: var(--col-text-deut);
+      }
+  }
+
+  #playback-progress {
+    p {
+      @include font-ui;
+    }
+  }
+}
+
+
+button {
+  height: 1.5rem;
+  aspect-ratio: 1;
+}
+
+</style>
