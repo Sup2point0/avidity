ce54edf883f7936b8030bd1b693bf196e03a0672
Sup#2.0 <sup2point0@gmail.com>
2024-10-23 20:26:44 +0100

    add `PlaylistTag`

---
+++ src/parts/playlist-tag.svelte
@@ -0,0 +1,27 @@
+<!-- @component PlaylistTag
+ 
+A coloured tag representing a playlist a track belongs to.
+-->
+
+<script lang="ts">
+
+import { nav } from "#scripts/stores";
+import { find_playlist } from "#scripts/utils";
+
+export let playlist: string;
+
+
+let data = find_playlist(playlist);
+
+</script>
+
+
+<button class="playlist-tag"
+  style:background-color={data?.colour ?? "var(--col-flavour)"}
+  on:click={() => {
+    $nav.page = "lists";
+    $nav.selected_playlist = playlist;
+  }}
+>
+  {data?.name ?? playlist}
+</button>
