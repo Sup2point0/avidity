85c8257ddb5cf8458ed2ed3cf1ad7fee52e76264
Sup#2.0 <sup2point0@gmail.com>
2024-10-24 17:44:21 +0100

    add `PlaybackDetails`

---
+++ src/parts/playback-details.svelte
@@ -0,0 +1,33 @@
+<!-- @component TrackDetails
+
+A pane for viewing and editing details of the selected track?.
+-->
+
+<script>
+
+import { playback } from "#scripts/stores";
+import { find_track } from "#scripts/utils";
+
+import TrackRow from "#parts/track-row.svelte";
+
+</script>
+
+
+<div class="part">
+  {#each $playback.queue.entries() as [i, track]}
+    <TrackRow {track} ctx="queue" idx={i} />
+  {/each}
+</div>
+
+
+<style lang="scss">
+
+.part {
+  max-width: 40vw;
+  flex-grow: 0.5;
+  height: 100%;
+  padding: 1rem;
+  overflow-y: scroll;
+}
+
+</style>
