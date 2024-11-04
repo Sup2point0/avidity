53532bf1eb36c617473719ab651e9f542449f5dd
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-04 11:36:23 +0000

    implement track filtering

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
 
+import { Tracks } from "#scripts/data";
+import { playback, search } from "#scripts/stores";
+import { find_track, filter_tracks } from "#scripts/utils";
+
 import TrackRow from "#parts/track-row.svelte";
 
-import play_exec from "#scripts";
-import { Tracks } from "#scripts/data";
-import { playback } from "#scripts/stores";
+
+$: tracks = Object.keys($Tracks).map(shard => find_track(shard));
+$: filtered = $search.dirty ? filter_tracks(tracks, $search) : tracks;
 
 </script>
 
 
 <main>
-  {#each Object.keys($Tracks) as track}
+  {#each filtered as track}
     <TrackRow track={track} />
   {/each}
 </main>
-
-
-<!-- <section>
-  <h2> debug </h2>
-  <pre>play_exec: {JSON.stringify(play_exec, null, 2)} </pre>
-  <pre>$playback: {JSON.stringify($playback, null, 2)} </pre>
-</section> -->
 
 
 <style lang="scss">
@@ -34,22 +31,4 @@ main {
   scrollbar-color: var(--col-text-deut) var(--col-back);
   scrollbar-width: thick;
 }
-
-  section {
-    padding: 1rem;
-    position: absolute;
-    bottom: 0;
-    left: 1rem;
-    color: white;
-    background-color: rgb(0 0 0 / 90%);
-
-    &:hover {
-      display: none;
-    }
-
-    > * {
-      margin: 1rem;
-      display: block;
-    }
-  }
 </style>
