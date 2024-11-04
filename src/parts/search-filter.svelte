ef77aea4f8c8bbc4b695b2482d8bc06a80bf30bc
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-04 11:09:06 +0000

    add `SearchFilter`

---
+++ src/parts/search-filter.svelte
@@ -0,0 +1,52 @@
+<!-- @component SearchFilter
+
+A button that activates a search filter.
+-->
+
+<script lang="ts">
+
+import { search } from "#scripts/stores";
+
+export let text: string;
+export let filter: string | undefined = undefined;
+
+
+$: active = $search[filter];
+
+</script>
+
+
+<button class="part" class:active
+  on:click={() => {if (filter) {
+    $search[filter] = !active;
+  }}
+>
+  {text}
+</button>
+
+
+<style lang="scss">
+
+.part {
+  min-width: 2rem;
+  padding: 0.5em 1em;
+
+  @include font-ui;
+  background-color: var(--col-card);
+  border-radius: 0.5em;
+
+  &.active {
+    background-color: var(--col-card-hover);
+    border-radius: 0.25em;
+  }
+
+  &:hover, &:focus {
+    background-color: var(--col-card-click);
+  }
+
+  &:active {
+    background-color: var(--col-prot);
+  }
+}
+
+</style>
