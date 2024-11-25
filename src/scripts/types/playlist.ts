3f7cddc0925ddb2580a3d05c303b1d7d2f3bcac7
Sup#2.0 <sup2point0@gmail.com>
2024-11-25 16:28:29 +0000

    develop playlist

--- src/scripts/types/playlist.ts
+++ src/scripts/types/playlist.ts
@@ -1,13 +1,15 @@
-import type { Track } from "./track";
-
-
+/**
+ * Represents a playlist of tracks.
+ */
 export class Playlist
 {
   public shard: string;
   public kind: "album" | "tag";
   public name: string;
   public colour: string;
-  public tracks: Track[];
+  public cover: string | null;
+  public tracks: string[];
+  public featured: string | null;
   public plays: number;
 
   constructor(
@@ -16,10 +18,14 @@ export class Playlist
   )
   {
     this.shard = shard;
+
     this.kind = data?.kind ?? "tag"
     this.name = data?.name ?? shard;
-    this.colour = data?.col ?? "var(--col-deut)"
-    this.tracks = [];
+    this.colour = data?.col ?? "var(--col-deut)";
+    
+    this.tracks = data?.tracks ?? [];
+    this.featured = data?.featured ?? this.tracks.at(0) ?? null;
+    this.cover = data?.cover ?? this.tracks.at(0);
     this.plays = data?.plays ?? 0;
   }
 }
